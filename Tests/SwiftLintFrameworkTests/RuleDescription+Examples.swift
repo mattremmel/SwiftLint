936714ca9de7ef16ddf25ab9d5410f60a79622d1
freak4pc <freak4pc@gmail.com>
2018-04-25 11:36:12 +0300

    Add RuleDescription.with(nonTriggeringExamples:triggeringExamples:)

--- Tests/SwiftLintFrameworkTests/RuleDescription+Examples.swift
+++ Tests/SwiftLintFrameworkTests/RuleDescription+Examples.swift
@@ -9,7 +9,8 @@
 import SwiftLintFramework
 
 extension RuleDescription {
-    func with(nonTriggeringExamples: [String]) -> RuleDescription {
+    func with(nonTriggeringExamples: [String],
+              triggeringExamples: [String]) -> RuleDescription {
         return RuleDescription(identifier: identifier,
                                name: name,
                                description: description,
@@ -20,15 +21,14 @@ extension RuleDescription {
                                deprecatedAliases: deprecatedAliases)
     }
 
+    func with(nonTriggeringExamples: [String]) -> RuleDescription {
+        return with(nonTriggeringExamples: nonTriggeringExamples,
+                    triggeringExamples: triggeringExamples)
+    }
+
     func with(triggeringExamples: [String]) -> RuleDescription {
-        return RuleDescription(identifier: identifier,
-                               name: name,
-                               description: description,
-                               kind: kind,
-                               nonTriggeringExamples: nonTriggeringExamples,
-                               triggeringExamples: triggeringExamples,
-                               corrections: corrections,
-                               deprecatedAliases: deprecatedAliases)
+        return with(nonTriggeringExamples: nonTriggeringExamples,
+                    triggeringExamples: triggeringExamples)
     }
 
     func with(corrections: [String: String]) -> RuleDescription {
